4cede3c365a40338fe01d717d1c3f48541d1da09
Koninski <139650678+Koninski@users.noreply.github.com>
2023-10-28 16:37:05 +0300

    changes models
    В модель course добавлено поле для файла видео. и функция сохранения.

--- course/models.py
+++ course/models.py
@@ -5,6 +5,10 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
 
+def get_path_save1(instance,filename):
+    str1 = '{0}\\{1}'.format(instance.author, filename)     # Только функцию нужно доделать чтобы она не пыталась создать
+    return str1                                             # папку с таким же именем
+
 class Course(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField() #max_length
@@ -14,6 +18,7 @@ class Course(models.Model):
     category = models.ForeignKey('course.Category',
                                  on_delete=models.SET_NULL,
                                  null=True)
+    video = models.FileField(upload_to=get_path_save1, verbose_name='Файл видео урока')
                                  # Или models.SET_DEFAULT категорию "без категории"
     #image = models.ImageField() #upload_to
 
