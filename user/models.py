9e796d3f3c1c2ad4d128ab0c96134e7fac8feb36
golubovv <golubbovv@gmail.com>
2023-10-28 15:49:53 +0300

    added .gitignore, .env and postgres + models bugfix

--- user/models.py
+++ user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from lesson.models import Lesson
 
 class User(models.Model):
     name = models.CharField(max_length=50)
@@ -8,15 +7,17 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(unique=True, max_length=20) # Поле и длина под вопросом
     photo = models.ImageField() #upload_to
-    likes = models.ManyToManyField(Lesson)
-    subs = models.ManyToManyField(User)
-    favorites = models.ManyToManyField(Lesson)
+    subs = models.ManyToManyField('user.User')
+    likes = models.ManyToManyField('lesson.Lesson',
+                                   related_name='liked_lessons')
+    favorites = models.ManyToManyField('lesson.Lesson',
+                                       related_name='favorite_lessons')
 
 class History(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE)
-    lesson = models.ForeignKey(Lesson,
+    lesson = models.ForeignKey('lesson.Lesson',
                                on_delete=models.CASCADE)
     # Одна запись - один урок в истории, легко сортировать по дате просмотра
     ########## Или
